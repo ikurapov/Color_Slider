ffd7d8946f0e5bc262076ed9b516168b3962e967
Kurapov <mastix2p@gmail.com>
2022-10-25 00:34:59 +0300

    keyUp & keyDown

--- js/app.js
+++ js/app.js
@@ -15,6 +15,14 @@ upBtn.addEventListener('click', () => {
 downBtn.addEventListener('click', () => {
      changeSlide('down')
 })
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up')
+    }
+    if (event.key === 'ArrowDown') {
+        changeSlide('down')
+    }
+})
 
 function changeSlide(direction) {
     if (direction === 'up') {
